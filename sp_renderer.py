5307385a177eb694d40654d5c21c81328f740e0f
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2021-11-09 02:26:47 +0500

    Changed resolution scale to match font sizes

--- sp_renderer.py
+++ sp_renderer.py
@@ -2,12 +2,12 @@ import wand.image
 from wand.color import Color
 from xml.sax.saxutils import escape
 template = """pango:<span size="{}" color="#{}" font_family="{}" font_features="liga">{}</span>"""
-def display(text, resolution=4):
+def display(text, resolution=133):
     NUM_LINES = len(text.split("\n"))
-    WIDTH = resolution*400
-    HEIGHT = resolution*50*(NUM_LINES+1)
-    DISK_SIZE = resolution*1.4
-    FONT_SIZE = resolution*25600
+    WIDTH = resolution*12
+    HEIGHT = round(resolution*1.5*(NUM_LINES+1))
+    DISK_SIZE = round(resolution*0.042)
+    FONT_SIZE = resolution*770
     text = escape(escape(text)) # yes you have to escape twice. blame pango
     with wand.image.Image() as image_text:
         with wand.image.Image() as image_shadow:
