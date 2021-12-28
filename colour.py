341fa87052cc2c6a68b7b0bd518d65dcc2960b52
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2021-12-28 12:10:24 +0500

    oops

--- colour.py
+++ colour.py
@@ -1,7 +1,7 @@
 from discord import Colour
 
 def discord_colours(dictionary):
-    return {k: Colour.from_rgb(rgb_tuple(v)) for k, v in dictionary.items()}
+    return {k: Colour.from_rgb(*rgb_tuple(v)) for k, v in dictionary.items()}
 
 def rgb_tuple(value):
     return tuple(bytes.fromhex(value))
