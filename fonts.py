fb8579573c59f035236fce9b06db4d83628de329
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2022-07-21 13:34:50 +0500

    Added check for filename

--- fonts.py
+++ fonts.py
@@ -3,4 +3,4 @@ from jasima import bundle
 
 fonts = {font["name_short"]: "fonts/" + font["filename"]
          for font in bundle["fonts"].values()
-         if exists("fonts/" + font["filename"])}
+         if "filename" in font if exists("fonts/" + font["filename"])}
