17e5053abdbd0cfc24cec77a91b87bb8644c6015
Gregory Danielson <gregory.danielson3@gmail.com>
2023-02-07 17:29:16 -0600

    Add dynamic relexer pulled (badly) from jasima

--- src/ilo/cogs/relex/cog.py
+++ src/ilo/cogs/relex/cog.py
@@ -3,37 +3,24 @@ from discord.commands import slash_command
 from discord.ext import commands
 
 from ilo.defines import text
-from ilo.preferences import preferences
 from ilo.relexer import relex
-
-
-def setup(bot):
-    bot.add_cog(CogRelex(bot))
 
 
 class CogRelex(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @slash_command(name="relex", description=text["DESC_RELEX"])
+    @slash_command(name="relex", description=text["DESC_RELEX_EN"])
     @option(name="input", description=text["DESC_RELEX_INPUT"])
-    # @option(
-    #     name="lang",
-    #     description=text["DESC_RELEX_LANGUAGE_OPTION"],
-    #     # choices=get_languages_for_slash_commands(),
-    #     default="",
-    # )
-    async def slash_relex(self, ctx, input):  # , lang):
-        await relex_command(ctx, input)  # , lang)
+    async def slash_relex_en(self, ctx, input):
+        await relex_command(ctx, input, "en")
+
+    @slash_command(name="mama", description=text["DESC_RELEX_MAMA"])
+    @option(name="input", description=text["DESC_RELEX_INPUT"])
+    async def slash_relex_etym(self, ctx, input):
+        await relex_command(ctx, input, "etym")
 
 
-async def relex_command(ctx, input: str):  # , lang: Optional[str]):
-    # TODO: re-add lang option when more langs are available?
-    # lang = preferences.get(str(ctx.author.id), "language") if not lang else lang
-    # lang = "en" if lang == "tok" else lang
-    # coalesce to 1. param lang 2. preferences lang 3. not tok 4. en default
-
-    lang = "en"
-    relexed = relex(input, lang)
-
+async def relex_command(ctx, input: str, method: str):
+    relexed = relex(input, method)
     await ctx.respond(relexed)
--- src/ilo/relexer.py
+++ src/ilo/relexer.py
@@ -1,22 +1,77 @@
-from ilo.defines import relex_map
+import re
+from collections.abc import Callable
+
+from ilo.jasima import bundle, get_word_entry
 from ilo.tokenizer import SENT_DELIMITERS, tokenize
 
+bundle = bundle["data"]
 
-def get_relex(word: str, lang: str) -> str:
-    found = relex_map.get(word)
-    word = found[lang] if found else word
+SYMBOL_RE = r"[^a-zA-Z]*"
+SYMBOL_RE = re.compile(SYMBOL_RE)
+
+ETYM_RE = r"^(?P<word>.+) (?:‘(?P<definition>.*)’)?(?:\((?P<alttext>.*)\))?"
+# TODO: incomplete, waiting on consistent format in jasima
+MULTI_ETYM_RE = rf""
+
+EN_SPECIAL_CASES = {
+    "li": "is",
+    "e": "the",
+    "ju": "reserved",
+    "ku": "interact with Toki Pona Dictionary",
+    "lu": "reserved",
+    "nu": "reserved",
+    "pu": "interact with Toki Pona: The Language of Good",
+    "su": "reserved",
+    "u": "reserved",
+}
+
+
+def __filter_only_alpha(text: str) -> str:
+    return re.sub(SYMBOL_RE, "", text)
+
+
+def __get_nth_word(defin: str, n: int = 0) -> str:
+    return defin.split(" ")[n]
+
+
+def relex_word_en(word: str) -> str:
+    """
+    Relex a given word with the following priority order:
+    1. special cases
+    2. first word of ku data
+    3. first word of definition
+    4. original input (do nothing)
+
+    This is not reasonable to reverse at present. jasima needs a relex column.
+    """
+    found = bundle.get(word)
+    if word in EN_SPECIAL_CASES:
+        return EN_SPECIAL_CASES[word]
+    fetch_from = found.get("ku_data") or found["def"]["en"] if found else word
+    word = __filter_only_alpha(__get_nth_word(fetch_from))
     return word
 
 
-def relex(input: str, lang: str):
-    # TODO: derive relex from jasima defs somehow?
+def __clean_etym(etym: str, multiple: bool = False) -> str:
+    n = 1 if multiple else 0
+    return __get_nth_word(etym, n)
 
-    lang = "en"
 
+def relex_word_etym(word: str) -> str:
+    found = bundle.get(word)
+    etymology = found.get("etymology")
+    if not etymology or etymology in {"∅", "?", "unknown"}:
+        return word
+    source = found["source_language"]
+    candidate = __clean_etym(etymology, source.startswith("multiple"))
+    return candidate
+
+
+def __sent_relex(input: str, relex_func: Callable) -> str:
     sents_of_words = tokenize(input)
     reconstructed = ""
     for sent in sents_of_words:
-        to_reconstruct = [get_relex(word, lang) for word in sent[:-1]]
+        to_reconstruct = [relex_func(word) for word in sent[:-1]]
         reconstructed += " ".join(to_reconstruct)
 
         # the last is punct if punct is present
@@ -25,6 +80,22 @@ def relex(input: str, lang: str):
         if final in SENT_DELIMITERS:
             reconstructed += final + " "
         else:
-            reconstructed += " " + get_relex(final, lang)
+            reconstructed += " " + relex_func(final)
             # last item whatsoever
     return reconstructed
+
+
+def relex_sent_etym(input: str):
+    return __sent_relex(input, relex_word_etym)
+
+
+def relex_sent_en(input: str):
+    return __sent_relex(input, relex_word_en)
+
+
+RELEX_FUNC_MAP = {"en": relex_sent_en, "etym": relex_sent_etym}
+
+
+# mostly for Discord's benefit
+def relex(input: str, method: str):
+    return RELEX_FUNC_MAP[method](input)
