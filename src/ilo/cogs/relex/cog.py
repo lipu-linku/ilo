00e456159a2568172db89e4654424b93da48848c
Gregory Danielson <gregory.danielson3@gmail.com>
2024-02-17 16:53:58 -0600

    feat: Support spoilers in /relex, /mama

--- src/ilo/cogs/relex/cog.py
+++ src/ilo/cogs/relex/cog.py
@@ -4,6 +4,7 @@ from discord.ext.commands import Cog
 from ilo.cog_utils import Locale
 from ilo.cogs.nimi.cog import Literal
 from ilo.relexer import relex
+from ilo.strings import spoiler_text
 
 
 class CogRelex(Cog):
@@ -14,15 +15,19 @@ class CogRelex(Cog):
 
     @locale.command("relex")
     @locale.option("relex-text")
-    async def slash_relex_en(self, ctx: ApplicationContext, text: str):
-        await relex_command(ctx, text, "en")
+    @locale.option("relex-spoiler")
+    async def slash_relex_en(self, ctx: ApplicationContext, text: str, spoiler: bool = False):
+        await relex_command(ctx, text, spoiler, "en")
 
     @locale.command("mama")
     @locale.option("mama-text")
-    async def slash_relex_etym(self, ctx: ApplicationContext, text: str):
-        await relex_command(ctx, text, "etym")
+    @locale.option("mama-spoiler")
+    async def slash_relex_etym(self, ctx: ApplicationContext, text: str, spoiler: bool = False):
+        await relex_command(ctx, text, spoiler, "etym")
 
 
-async def relex_command(ctx, text: str, method: Literal["en", "etym"]):
+async def relex_command(ctx, text: str, spoiler: bool = False, method: Literal["en", "etym"] = "en"):
     relexed = relex(text, method)
+    if spoiler:
+        relexed = spoiler_text(relexed)
     await ctx.respond(relexed)
