eff16c9325dd9f90cb2f92451e2efc4c6707832c
Gregory Danielson <gregory.danielson3@gmail.com>
2024-03-15 13:23:51 -0500

    fix: Add line spacing to sp generation to avoid overlapping cartouches on newlines
    This makes lines obnoxiously tall when a cartouch is absent, but in fairness, this helps readability and we would need to do a mess of probably impossible things to detect names and react accordingly.

--- src/ilo/sitelen.py
+++ src/ilo/sitelen.py
@@ -1,18 +1,23 @@
 import io
+from typing import Tuple
 
 from PIL import Image, ImageDraw, ImageFont
 
 
 # by jan Tepo
-def display(text, font_path, font_size, color):
-    STROKE_WIDTH = round(font_size / 133 * 5)
+def display(text: str, font_path: str, font_size: int, color: Tuple[int, int, int]):
+    STROKE_WIDTH = round((font_size / 133) * 5)
+    LINE_SPACING = round((font_size / 11) * 2)
+
     font = ImageFont.truetype(font_path, font_size)
     d = ImageDraw.Draw(Image.new("RGBA", (0, 0), (0, 0, 0, 0)))
     x, y, w, h = d.multiline_textbbox(
         (0, 0), text, stroke_width=STROKE_WIDTH, font=font
     )
     image = Image.new(
-        "RGBA", (x + w + STROKE_WIDTH * 2, y + h + STROKE_WIDTH * 2), (0, 0, 0, 0)
+        "RGBA",
+        (x + w + STROKE_WIDTH * 2, y + h + STROKE_WIDTH * 2 + LINE_SPACING),
+        (0, 0, 0, 0),
     )
     d = ImageDraw.Draw(image)
     d.multiline_text(
@@ -20,6 +25,7 @@ def display(text, font_path, font_size, color):
         text,
         font=font,
         fill=color,
+        spacing=LINE_SPACING,
         stroke_width=STROKE_WIDTH,
         stroke_fill=(0x36, 0x39, 0x3F),
     )
