38d40edd286d26a827c7d63f2bdd2790e26f8ab8
Gregory Danielson <gregory.danielson3@gmail.com>
2024-03-15 19:30:18 -0500

    feat: Dynamically switch outline based on user's selected color
    Notes: In the interest of not over-correcting, colors very near to my selected minimum are still not distinguished enough.
    To do this correctly, we'd have to go by distance from the outline shade of black.

--- src/ilo/sitelen.py
+++ src/ilo/sitelen.py
@@ -3,8 +3,28 @@ from typing import Literal, Tuple
 
 from PIL import Image, ImageDraw, ImageFont
 
-OUTLINE_COLOR = (0x36, 0x39, 0x3F)
-BG_MAP = {"outline": (0, 0, 0, 0), "background": (0x36, 0x39, 0x3F, 0xFF)}
+BLACK = (0x36, 0x39, 0x3F, 0xFF)
+WHITE = (0xF0, 0xF0, 0xF0, 0xFF)
+TRANSPARENT = (0, 0, 0, 0)
+MINIMUM_BRIGHTNESS = 0x40
+
+
+def get_bg_stroke_colors(
+    color: Tuple[int, int, int],
+    bgstyle: Literal["outline"] | Literal["background"],
+):
+    bg_color = BLACK
+    stroke_color = BLACK
+
+    # if (sum(color) / len(color)) < MINIMUM_BRIGHTNESS:
+    if all(map(lambda c: c < MINIMUM_BRIGHTNESS, color)):
+        bg_color = WHITE
+        stroke_color = WHITE
+
+    if bgstyle == "outline":
+        bg_color = TRANSPARENT
+
+    return bg_color, stroke_color
 
 
 # by jan Tepo
@@ -18,6 +38,7 @@ def display(
     STROKE_WIDTH = round((font_size / 133) * 5)
     LINE_SPACING = round((font_size / 11) * 2)
 
+    bg_color, stroke_color = get_bg_stroke_colors(color, bgstyle)
     font = ImageFont.truetype(font_path, font_size)
     d = ImageDraw.Draw(Image.new("RGBA", (0, 0), (0, 0, 0, 0)))
     x, y, w, h = d.multiline_textbbox(
@@ -26,7 +47,7 @@ def display(
     image = Image.new(
         mode="RGBA",
         size=(x + w + STROKE_WIDTH * 2, y + h + STROKE_WIDTH * 2 + LINE_SPACING),
-        color=BG_MAP[bgstyle],
+        color=bg_color,
     )
     d = ImageDraw.Draw(image)
     d.multiline_text(
@@ -36,7 +57,7 @@ def display(
         fill=color,
         spacing=LINE_SPACING,
         stroke_width=STROKE_WIDTH,
-        stroke_fill=OUTLINE_COLOR,
+        stroke_fill=stroke_color,
     )
     img_out = io.BytesIO()
     image.save(img_out, format="PNG")
