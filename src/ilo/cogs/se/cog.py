9768df7a80f21c39c46be8358a3f2a69c1a20b28
Gregory Danielson <gregory.danielson3@gmail.com>
2024-08-23 13:59:04 -0500

    refactor: type-ify the se cog

--- src/ilo/cogs/se/cog.py
+++ src/ilo/cogs/se/cog.py
@@ -1,49 +1,65 @@
 # from pozzei
 
 import re
+from typing import Literal, cast
 
-from discord import ApplicationContext
+from discord import ApplicationContext, Bot
 from discord.ext.commands import Cog
 
 from ilo.cog_utils import Locale, load_file
 from ilo.data import get_word_data
 from ilo.strings import spoiler_text
 
+UserChoices = Literal["sitelen jelo"] | Literal["sitelen pilin"]
+LinkuEmojiReprs = Literal["sitelen_jelo"] | Literal["sitelen_emosi"]
+choice_to_linku_repr: dict[UserChoices, LinkuEmojiReprs] = {
+    "sitelen jelo": "sitelen_jelo",
+    "sitelen pilin": "sitelen_emosi",
+}
+
 
 class CogSe(Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: Bot):
         self.bot = bot
 
     locale = Locale(__file__)
 
     @locale.command("se")
     @locale.option("se-text")
-    @locale.option("se-system", choices=["sitelen jelo","sitelen pilin"])
+    @locale.option("se-system", choices=list(choice_to_linku_repr.keys()))
     @locale.option("se-spoiler")
-    async def slash_se(self, ctx: ApplicationContext, text: str, system: str, spoiler: bool = False):
-        await se(ctx, text, spoiler)
+    async def slash_se(
+        self,
+        ctx: ApplicationContext,
+        text: str,
+        system: str = "sitelen jelo",
+        spoiler: bool = False,
+    ):
+        await se(ctx, text, cast(UserChoices, system), spoiler)
 
 
-async def se(ctx: ApplicationContext, string: str, system: str, spoiler: bool = False):
+async def se(
+    ctx: ApplicationContext, string: str, system: UserChoices, spoiler: bool = False
+):
     if len(string) > 500:
         response = (
             "Message is too long. Please try to keep messages below 500 characters."
         )
     string = clean_string(string)
-    system = clean_string(system)
-    if system=="sitelen pilin":
-        chosen_system="sitelen_pilin"
-    else:
-        chosen_system="sitelen_jelo"
+    if not string:
+        _ = await ctx.respond("Input became empty. Please provide a proper input.")
 
-    if string == "":
-        response = "Resulting string is empty. Please provide a proper input."
+    chosen_system = choice_to_linku_repr[system]
 
-    response = " ".join(list(map(sitelen_emosi, [string.split(),chosen_system])))
+    chars: list[str] = []
+    for word in string.split():
+        result = sitelen_emosi(word, chosen_system)
+        chars.append(result)
+    response = " ".join(chars)
 
     if spoiler:
         response = spoiler_text(response)
-    await ctx.respond(response)
+    _ = await ctx.respond(response)
 
 
 def clean_string(string: str):
@@ -58,22 +74,26 @@ def clean_string(string: str):
     return clean_string
 
 
-def sitelen_emosi(pref: list[list[str],str]):
-    word=pref[0]
-    chosen_system=pref[1]
+def sitelen_emosi(word: str, chosen_system: LinkuEmojiReprs) -> str:
     word_data = get_word_data(word)
-    if word_data:
-        if chosen_system=="sitelen_pilin":
-            sitelen = word_data["representations"].get("sitelen_emosi")
-        else:
-            sitelen = word_data["representations"].get("sitelen_jelo")[0]
-        if sitelen:
-            return sitelen
+    sitelen_ken: str | list[str] | None = (
+        word_data.get("representations", {}).get(chosen_system) if word_data else None
+    )
+    if not word_data or not sitelen_ken:
+        return fallback_emoji(word)
 
-    chars = []
+    if isinstance(sitelen_ken, list):
+        # TODO: let user choose among the list
+        sitelen_ken = sitelen_ken[0]
+
+    return sitelen_ken
+
+
+def fallback_emoji(word: str) -> str:
+    chars: list[str] = []
     for letter in word:
         chars.append(extraemoji[letter])
     return " ".join(chars)
 
 
-extraemoji = load_file(__file__, "extraemoji.json")
+extraemoji = cast(dict[str, str], load_file(__file__, "extraemoji.json"))
